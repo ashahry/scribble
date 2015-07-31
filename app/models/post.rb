049fba5e364154c7386d3db66c873e71795847c9
Arash Shahry <arash.shahry@gmail.com>
2015-07-31 14:32:19 -0400

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  has_many :comments, dependent: :destroy #comments get deleted if posts are deleted.
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
 end
